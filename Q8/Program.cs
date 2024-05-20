6d88f18def25d3774a1ee63eb5962f932be6f110
D2_suyash_84016 <patilsuyash1177@gmail.com>
2024-05-20 17:45:25 +0530

    assignment-Q8  completed

---
+++ Q8/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Q8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   Employee employee = new Employee();
+            employee.accept();
+            employee.print();
+            Employee employee1 = new Employee();
+            employee1.accept();
+            employee1.print();
+        }
+    }
+
+    class Employee
+    {
+        int count ;
+        enum DepartmentType
+        {
+            IT,
+            HR,
+            Finance,
+            Marketing
+        }
+       
+
+        private static int _id  ;
+        private double _salary;
+        private string _designation;
+        private DepartmentType _department;
+
+        public Employee()
+        {
+            _id++;
+        }
+
+
+
+
+
+        public string designation
+        {
+            get { return _designation; }
+            set { _designation = value; }
+        }
+
+
+        public double salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+
+        public int id
+        {
+            get { return _id = id; }
+            set { _id= value; }
+        }
+
+        public void accept()
+        {
+            Console.WriteLine("enter salary");
+            _salary=Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter designation:");
+            _designation = Console.ReadLine();
+
+            Console.WriteLine("Enter department (IT, HR, Finance, Marketing):");
+            _department = Enum.Parse<DepartmentType>(Console.ReadLine());
+        }
+
+        public void print()
+        {
+            Console.WriteLine("id=" + _id);
+            Console.WriteLine("sal=" + salary);
+            Console.WriteLine("designagtion=" + designation);
+            Console.WriteLine("department=" + _department);
+                }
+    }
+}
